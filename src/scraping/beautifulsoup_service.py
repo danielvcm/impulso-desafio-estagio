0311fb4e42d32d91c943ff39234c025399ac4646
Daniel Miranda <danielvcmiranda@gmail.com>
2020-08-28 20:56:38 -0300

    renomeando e incluindo método de achar url do kibana

--- src/scraping/beautifulsoup_service.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-class BeautifulSoupService:
-    def __init__(self, html):
-        self.html = BeautifulSoup(html,'html.parser')
-    
-    def get_text_from_response(self):
-        soup = self.html.find('code', {'data-test-subj':"inspectorResponseBody"})
-        text = soup.get_text()
-        return text
